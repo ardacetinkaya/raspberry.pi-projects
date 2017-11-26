0dff6aecd38e711c6c3385ff247b2f29414f827c
Arda Cetinkaya <Arda@Ardas-MacBook-Pro.local>
2017-11-26 21:44:44 +0300

    New motor is added also input controls are improved

--- BrickPi/MotorManagement.py
+++ BrickPi/MotorManagement.py
@@ -8,7 +8,7 @@ class _Getch:
             old_settings = termios.tcgetattr(fd)
             try:
                 tty.setraw(sys.stdin.fileno())
-                ch = sys.stdin.read(3)
+                ch = sys.stdin.read(1)
             finally:
                 termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
             return ch
@@ -19,26 +19,35 @@ def get(BP):
             k=inkey()
             if k!='':break
 
-    if k=='\x1b[A':
+    if k=='w':
         print "up"
         BP.set_motor_power(BP.PORT_B, 100)
         BP.set_motor_power(BP.PORT_C, 100)
-    elif k=='\x1b[B':
+    elif k=='s':
         print "down"
         BP.set_motor_power(BP.PORT_B, -100)
         BP.set_motor_power(BP.PORT_C, -100)
-    elif k=='\x1b[C':
+    elif k=='d':
         print "right"
         BP.set_motor_power(BP.PORT_B, 0)
         BP.set_motor_power(BP.PORT_C, 100)
-    elif k=='\x1b[D':
+    elif k=='a':
         print "left"
         BP.set_motor_power(BP.PORT_B, 100)
         BP.set_motor_power(BP.PORT_C, 0)
+    elif k=='q':
+        sys.exit(0)
+    elif k=='x':
+        BP.set_motor_power(BP.PORT_A, 5)
+    elif k=='z':
+        BP.set_motor_power(BP.PORT_A,-5)
+    elif k=='c':
+        BP.set_motor_power(BP.PORT_A,0)
     else:
         print "stop"
         BP.set_motor_power(BP.PORT_B, 0)
         BP.set_motor_power(BP.PORT_C, 0)
+        BP.set_motor_power(BP.PORT_A, 0)
 
     time.sleep(0.02)
 
@@ -55,10 +64,6 @@ def main():
     try:
         while True:
             result = get(BP)
-            print(result)
-            if(result==-1):
-                BP.reset_all()
-                break
 
     except (KeyboardInterrupt, SystemExit):
         print '\n...Program Stopped Manually!!!'
